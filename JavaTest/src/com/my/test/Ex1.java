736751bdcdf49cc772e9792bd343e1bda078793f
KJH <tobajh@gmail.com>
2022-03-30 15:45:02 +0900

    String Example 1,2

--- JavaTest/src/com/my/test/Ex1.java
+++ JavaTest/src/com/my/test/Ex1.java
@@ -1,11 +1,17 @@
 package com.my.test;
 
 import java.util.Scanner;
-
+/**
+ * 
+ * @author jhkim
+ * String(문자열 관련 테스트)
+ * Solution1 : 문자열에서 문자 갯수 찾기
+ * Solution2 : 대소문자 변환 
+ *
+ */
 public class Ex1 {
 	
-	//문자 갯수 찾기 
-	public int Solution(String str, char t) {
+	public int Solution1(String str, char t) {
 		int answer = 0;
 		
 		//대문자로 변경하기
@@ -27,13 +33,31 @@ public class Ex1 {
 		}
 		return answer;
 	}
+	
+	public String solution2(String str) {
+		String answer = "";
+		for(char x : str.toCharArray()) {
+			if(Character.isLowerCase(x)) answer+= Character.toUpperCase(x);
+			else answer += Character.toLowerCase(x);
+		}
+		return answer;
+	}
 
 	public static void main(String[] args) {
 		Ex1 T = new Ex1();
+		
+		/*[Solution1]
 		Scanner kb = new Scanner(System.in);
 		String str = kb.next();
 		char c = kb.next().charAt(0);
-		System.out.print(T.Solution(str, c));
+		System.out.print(T.Solution1(str, c));
+		*/
+		
+		//Solution2.
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution2(str));
+		
 	}
 
 }
