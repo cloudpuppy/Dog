65541d5bce6900a1f9163d106f7b7f7a9796c1d5
cloudpuppy <64085337+cloudpuppy@users.noreply.github.com>
2024-05-10 16:48:00 +0900

    Update MyServlet.java

--- JavaPractice/Server/MyServlet.java
+++ JavaPractice/Server/MyServlet.java
@@ -46,4 +46,48 @@ public class MyServlet extends HttpServlet {
 		res.setStatus(200);
 		res.getWriter().write(fname + " saved!");
 	}
+
+	void doPostSample2(){
+	String requestString = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+	CommandRequest requestData = this.gson.fromJson(requestString, CommandRequest.class);
+	switch (request.getPathInfo()) {
+	case "/fromServer":
+		List<String> result = new ArrayList<>();
+			for (String targetDevice : requestData.getTargetDevice()) {
+			DeviceInfo deviceInfo = this.deviceInfoMap.get(targetDevice);
+			String forwardCommand = this.serverCommandInfoMap.get(requestData.getCommand()).getForwardCommand();
+				String commandResponse = null;
+			try {
+				commandResponse = sendPostRequest(
+						String.format("http://%s:%d/fromEdge", deviceInfo.getHostname(), deviceInfo.getPort()),
+						new CommandRequest(forwardCommand, null, requestData.getParam()).toJson(gson));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			CommandResponse responseFromDevice = gson.fromJson(commandResponse, CommandResponse.class);
+			result.addAll(responseFromDevice.getResult());
+		}
+		response.setStatus(200);
+		response.getWriter().write(new CommandResponse(result).toJson(gson));
+			break;
+		}
+	}
+
+
+	private String sendPostRequest(String url, String content) throws Exception {
+	HttpClient httpClient = new HttpClient();
+	httpClient.start();
+	try {
+		org.eclipse.jetty.client.api.Request request = httpClient.POST(url);
+		request.header(HttpHeader.CONTENT_TYPE, "application/json");
+		request.content(new StringContentProvider(content, "utf-8"));
+		ContentResponse response = request.send();
+		return new String(response.getContent());
+	} catch (ExecutionException executionException) {
+		executionException.printStackTrace();
+	} finally {
+		httpClient.stop();
+	}
+	return null;
+	}
 }
